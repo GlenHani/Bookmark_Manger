9a70bcf72cd50c7e2b3e3c3c4a06eb6de8f01ee6
Chan <chanpreetsingh974@gmail.com>
2020-05-18 15:51:48 +0100

    First user story

--- lib/app.rb
+++ lib/app.rb
@@ -3,7 +3,18 @@ require 'sinatra/base'
 class BookmarkManager < Sinatra::Base
   get '/' do
     'Bookmark Manager'
+    erb :index
   end
+
+  get '/bookmarks/all' do
+    @bookmarks = [
+      "http://www.makersacademy.com",
+      "http://www.destroyallsoftware.com",
+      "http://www.google.com"
+     ]
+
+     erb :bookmarks
+  end 
 
   run! if app_file == $0
 end
