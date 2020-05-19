a892dd67268f632690a24b48f4bc646eb05dfe41
GlenHani <glen.hani@beis.gov.uk>
2020-05-19 15:51:16 +0100

    Section 9 - Set Up Env

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,9 +1,21 @@
 require './lib/bookmark'
+require './spec/setup_test_database'
 
 RSpec.describe Bookmark do
   describe '.all' do
     it 'returns a list of bookmarks' do
       bookmarks = Bookmark.all
+
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      setup_test_database
+
+
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
   
       expect(bookmarks).to include "http://www.makersacademy.com"
       expect(bookmarks).to include "http://www.destroyallsoftware.com"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
 RSpec.configure do |config|
 
 
@@ -28,6 +29,10 @@ RSpec.configure do |config|
   require 'capybara'
   require 'capybara/rspec'
   require 'rspec'
+
+
+  ENV['ENVIRONMENT'] = 'test'
+  
 
   # Tell Capybara to talk to BookmarkManager
   Capybara.app = BookmarkManager
@@ -60,6 +65,8 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
